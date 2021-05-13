d524cdce0245cd57d6a02cc4c0911d92e986a570
Mannuc-90 <manpreet.c1990@gmail.com>
2021-05-13 19:47:41 +0530

    Arrays -- committing main file to test max,intersect,reverse and insertAt methods

--- src/ds/array/Main.java
+++ src/ds/array/Main.java
@@ -4,17 +4,43 @@ public class Main {
     public static void main(String[] args) {
         Array numbers = new Array(5);
         numbers.insert(10);
+        numbers.insert(90);
+        numbers.insert(20);
         numbers.insert(20);
         numbers.insert(30);
+        numbers.insert(60);
+        numbers.insert(40);
         numbers.insert(40);
         numbers.insert(50);
-
-      //  numbers.removeAt(0);
-        System.out.println(numbers.indexOf(100));
-
+        numbers.insert(70);
+        //numbers.print();
 
 
-        numbers.print();
+        Array numbers2 = new Array(5);
+        numbers2.insert(10);
+        numbers2.insert(20);
+        numbers2.insert(30);
+        numbers2.insert(40);
+        numbers2.insert(40);
+        numbers2.insert(120);
 
+        //number2.print();
+        //numbers.removeAt(0);
+        //System.out.println(numbers.indexOf(100));
+
+        System.out.println("max : " + Array.max(numbers));
+        Array intersectArr = Array.intersect(numbers, numbers2);
+
+        System.out.println("Intersection Array elems : ");
+        intersectArr.print();
+
+        Array.reverse(intersectArr);
+        System.out.println("reversed : ");
+        intersectArr.print();
+
+        Array.insertAt(numbers2, 1, 90);
+        System.out.println("after insertAt ");
+        numbers2.print();
     }
+
 }
