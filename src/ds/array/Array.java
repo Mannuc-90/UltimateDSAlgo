fbaf23268d99d5fffc8945002dfc7c6dc8dea558
Mannuc-90 <manpreet.c1990@gmail.com>
2021-05-13 19:33:40 +0530

    Revert "Arrays --> implemented max,intersect,reverse and insertAt methods to Array class"
    This reverts commit ea53befc706e00ff2fc9b428732de48aa8abdf09.

--- src/ds/array/Array.java
+++ src/ds/array/Array.java
@@ -24,7 +24,6 @@ public class Array {
 
     /**
      * Parameterized constructor taking user defined capcity
-     *
      * @param capacity
      */
     public Array(int capacity) {
@@ -34,15 +33,15 @@ public class Array {
 
     /**
      * Method to insert items into array
+     * @param item
+     * If the number of elements exceeds capacity of array, then
+     *      -- capacity is doubled
+     *      -- a new array is initialized with new(doubled) capacity
+     *      -- items from old array is copied to new array
+     *      -- old array is pointed to new array
      *
-     * @param item If the number of elements exceeds capacity of array, then
-     *             -- capacity is doubled
-     *             -- a new array is initialized with new(doubled) capacity
-     *             -- items from old array is copied to new array
-     *             -- old array is pointed to new array
-     *             <p>
-     *             new item is placed at current index, ie length
-     *             index ie length is incremented
+     * new item is placed at current index, ie length
+     * index ie length is incremented
      */
     public void insert(int item) {
         if (length == capacity) {
@@ -59,12 +58,12 @@ public class Array {
 
     /**
      * Method to remove an element on specified index
-     *
-     * @param index If the index specified is invalid, ie, out of the array range, an exception is thrown
-     *              If the index specified is last index, ie index==length-1, no need to shift elems, just decrement the length
-     *              if its valid index<last index
-     *              --loop the array from index till last index(length-1)
-     *              --copy the index+1 values to index value in a loop
+     * @param index
+     * If the index specified is invalid, ie, out of the array range, an exception is thrown
+     * If the index specified is last index, ie index==length-1, no need to shift elems, just decrement the length
+     * if its valid index<last index
+     *      --loop the array from index till last index(length-1)
+     *      --copy the index+1 values to index value in a loop
      */
     public void removeAt(int index) {
         if (index < 0 || index >= length) {
@@ -81,13 +80,12 @@ public class Array {
 
     /**
      * Method to find the index of a value in array
-     *
      * @param item
      * @return index
      * loop through the array from 0 index till length and match each array value against the item
      * passed
-     * --If value found , return the index
-     * --If value not found, return -1
+     *      --If value found , return the index
+     *      --If value not found, return -1
      */
     public int indexOf(int item) {
         for (int i = 0; i < length; i++) {
@@ -108,122 +106,5 @@ public class Array {
         }
     }
 
-    /**
-     * Array Utility method to find the max item in array elements
-     *
-     * @return maxItem
-     * Algo :
-     * -- initialize maxItem =0
-     * -- loop through array and compare the current array item to maxItem
-     * -- If current array item > maxItem, assign maxItem = current array item
-     */
-    public static int max(Array array) {
-        int maxItem = 0;
-        for (int item : array.items) {
-            if (item > maxItem) {
-                maxItem = item;
-            }
-        }
-        return maxItem;
-    }
 
-    /**
-     * Method to find common elements in 2 arrays
-     *
-     * @param firstArray
-     * @param secondArray
-     * @return intersectArray
-     * algo :
-     * -- Find the max of of intersections ==> smaller array size
-     * -- loop through firstArray and for each array element for firstArray, call indexOf
-     * method on secondArray to find the first arrayElement exists in secondArray
-     * -- If indexOf!=-1(ie firstArray item found in secondArray) insert the item to
-     * intersectArray
-     */
-    public static Array intersect(Array firstArray, Array secondArray) {
-        int maxIntersects = 0;
-        if (firstArray.length < secondArray.length) {
-            maxIntersects = firstArray.length;
-        } else {
-            maxIntersects = secondArray.length;
-        }
-        Array intersectArray = new Array(maxIntersects);
-
-        for (int item : firstArray.items) {
-            if (secondArray.indexOf(item) != -1) {
-                intersectArray.insert(item);
-            }
-        }
-        return intersectArray;
-    }
-
-    /**
-     * Method to reverse a given array
-     * @param array
-     * algo: Two poimter technique
-     *      -- take the first index
-     *      -- take the last index
-     *      -- while firstIndex<lastIndex
-     *              -- swap array's elements first and last index values
-     *              -- increment firstIndex, decrement lastIndex
-     */
-    public static void reverse(Array array) {
-        int i = 0;
-        int j = array.length - 1;
-        while (i < j) {
-            swap(array, i, j);
-            i++;
-            j--;
-        }
-    }
-
-    /**
-     * Helper method to swap 2 values in array --  to be used by reverse method
-     * @param array
-     * @param i
-     * @param j
-     * @return
-     */
-    private static Array swap(Array array, int i, int j) {
-        int temp = array.items[i];
-        array.items[i] = array.items[j];
-        array.items[j] = temp;
-        return array;
-    }
-
-    /**
-     * Method to insert a value at a aparticlur index in array
-     * @param array
-     * @param index
-     * @param newItem
-     * logic :
-     *      corner case: If the length and capacity are same, we cant just insert new item
-     *          -- set new capacity to oldCapacity*2
-     *          -- create a new array with new capacity
-     *          -- copy prev array elements to new array
-     *          -- assign new array to array's object items array
-     *     loop the array from last element till the index provided
-     *          -- copy the last elem to position of last elem+1
-     *     once the index reach breaks the loop;
-     *     assign the value at index with value provided
-     *     increase the length of array
-     *
-     */
-    public static void insertAt(Array array, int index, int newItem){
-        if(array.length==array.capacity){
-           int newCapacity = array.capacity*2;
-           int[] newArray = new int[newCapacity];
-            for(int i=0;i<array.length;i++){
-                newArray[i] = array.items[i];
-            }
-            array.capacity = newCapacity;
-            array.items=newArray;
-        }
-
-        for(int i=array.length-1;i>=index;i--){
-            array.items[i+1] = array.items[i];
-        }
-        array.items[index] = newItem;
-        array.length++;
-    }
 }
